a5afceca562cde4ff701a6ab257c67f7d3da5efb
NokoDev <demozflava@gmail.com>
2022-05-21 22:56:04 +0200

    selected a few more elements

--- script.js
+++ script.js
@@ -3,9 +3,14 @@
 const toggle = document.getElementById("night")
 const body = document.getElementById("bodyy")
 const container = document.getElementsByClassName("container")
+const mailer = document.getElementById("email")
 
 toggle.addEventListener("click", () =>{
     console.log("clicked night mode")
     body.classList.toggle("body_night")
     
+})
+
+mailer.addEventListener("submit", () =>{
+    console.log("form submitted")
 })
